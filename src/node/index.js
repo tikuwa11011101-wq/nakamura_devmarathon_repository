4659877fafa90012e21c1c3c379ad14441279154
Rinta Nakamura <ri-nakamura@brexa.com>
2026-07-13 11:08:51 +0900

    27KM: main.yml作成、本番用DB接続情報の反映

--- src/node/index.js
+++ src/node/index.js
@@ -9,10 +9,10 @@ app.use(cors());
 
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: "user_5914", // PostgreSQLのユーザー名に置き換えてください
-  host: "db",
-  database: "crm_5914", // PostgreSQLのデータベース名に置き換えてください
-  password: "pass_5914", // PostgreSQLのパスワードに置き換えてください
+  user: "user_ri_nakamura", // PostgreSQLのユーザー名に置き換えてください
+  host: "localhost",
+  database: "db_ri_nakamura", // PostgreSQLのデータベース名に置き換えてください
+  password: "5Rw5YDaWc5jc", // PostgreSQLのパスワードに置き換えてください
   port: 5432,
 });
 
